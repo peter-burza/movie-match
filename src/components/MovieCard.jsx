0483488c73eb5bc8eac00502d68fcee9058e781b
Peter Burza <peterburza48@gmail.com>
2025-06-23 10:52:14 +0000

    Handling fetching and displaying data (unfinished)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,27 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-export default function MovieCard() {
+export default function MovieCard(props) {
+  const { movieData, setDetail, detail } = props;
+  const { original_title, overview, poster_path, backdrop_path, video } =
+    movieData || {};
 
-    return (
-        <button className="grid max-w-65 group aspect-[16/9] overflow-hidden rounded-xl border-1 place-items-center">
-            <div className="relative w-full ">
-                <img src="/2AndAHalfMan-ImgRef.jpg" alt="" className="object-cover w-full h-full" />
-                <div className="absolute bottom-0 left-0 w-full h-[25%] bg-gradient-to-t from-black to-125% translate-y-full group-hover:translate-y-0 transition-transform duration-300 flex items-center justify-center text-white">
-                    <span className="text-sm">2 And a Half Man</span>
-                </div>
-            </div>
-        </button>
-    )
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setDetail({ original_title, overview });
+        }}
+        className="relative max-w-55 group aspect-[12/15] overflow-hidden rounded-xl place-items-center appearance-none"
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/original${poster_path}`}
+          alt={original_title || "Movie poster"}
+          className="object-cover "
+        />
+        <div className="absolute bottom-0 left-0 w-full h-[20%] bg-gradient-to-t from-black to-transparent translate-y-full group-hover:translate-y-0 transition-transform duration-300 flex items-center justify-center text-white">
+          <span className="text-sm p-2">{original_title}</span>
+        </div>
+      </button>
+    </div>
+  );
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,85 +1,116 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
+import Modal from "./Modal";
 
 export default function Movies(props) {
-    const { searchedMovie } = props
-    const { data, setData } = useState(null)
-    const [loading, setLoading] = useState(false)
+  const { searchedMovie } = props;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [detail, setDetail] = useState(null);
 
-    const { original_title, poster_path, video } = data || {}
-
-    function renderFetchedData(fetchedData) {
-
+  useEffect(() => {
+    if (loading || !localStorage) return; // close guard..
+    //If there is any movie database
+    let cache = {};
+    if (localStorage.getItem("movie-database")) {
+      cache = JSON.parse(localStorage.getItem("movie-database"));
+    }
+    //If the searched movie is in the cche, otherwise fetch form API
+    if (searchedMovie in cache) {
+      setData(cache[searchedMovie]);
+      console.log("Found movie in cache: ", cache);
+      return;
     }
 
-    useEffect(() => {
-        if (loading || !localStorage) return // close guard..
-        //If there is any movie database
-        let cache = {}
-        if (localStorage.getItem('movie-databese')) {
-            cache = JSON.parse(localStorage.getItem('movie-databese'))
+    function fetchMovieData(searchedMovie) {
+      setLoading(true);
+
+      async function fetchMovieInfo(searchedMovie) {
+        try {
+          const url = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&include_adult=false&include_video=true&language=en-US&page=1`;
+          const options = {
+            method: "GET",
+            headers: {
+              accept: "application/json",
+              Authorization:
+                "Bearer .eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWRiZTMyZGYzZmZmNGFhNDk5MWIyNTI5MzY1YTE4ZSIsIm5iZiI6MTc1MDQwMDcxMi44NjA5OTk4LCJzdWIiOiI2ODU0ZmVjODRlMWJlZDUwNWUwY2Y2MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2YIFbjc7_BFrYz_skVjFzF-IGoeQ7Ku4RmjwUtXsLdE",
+            },
+          };
+          const res = await fetch(url, options);
+          const movieData = await res.json();
+
+          setData(movieData);
+          console.log("Fetched movie data from API:", movieData);
+          cache[searchedMovie] = movieData;
+          localStorage.setItem("movie-database", JSON.stringify(cache));
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setLoading(false);
         }
-        //If the searched movie is in the cche, otherwise fetch form API
-        if (searchedMovie in cache) {
-            setData(cache[searchedMovie])
-            console.log('Found movie in cache: ', searchedMovie)
-            return
-        }
+      }
+      fetchMovieInfo(searchedMovie);
 
-        async function fetchMovieData(searchedMovie) {
-            setLoading(true)
+      //   async function fetchMovieTrailer() {
+      //     const url =
+      //       "https://api.themoviedb.org/3/movie/movie_id/videos?language=en-US";
+      //     const options = {
+      //       method: "GET",
+      //       headers: {
+      //         accept: "application/json",
+      //         Authorization:
+      //           "Bearer .eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWRiZTMyZGYzZmZmNGFhNDk5MWIyNTI5MzY1YTE4ZSIsIm5iZiI6MTc1MDQwMDcxMi44NjA5OTk4LCJzdWIiOiI2ODU0ZmVjODRlMWJlZDUwNWUwY2Y2MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2YIFbjc7_BFrYz_skVjFzF-IGoeQ7Ku4RmjwUtXsLdE",
+      //       },
+      //     };
 
-            try {
-                const url = `https://api.themoviedb.org/3/search/movie?query=${searchedMovie}&include_adult=false&language=en-US&page=1`
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: 'Bearer .eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWRiZTMyZGYzZmZmNGFhNDk5MWIyNTI5MzY1YTE4ZSIsIm5iZiI6MTc1MDQwMDcxMi44NjA5OTk4LCJzdWIiOiI2ODU0ZmVjODRlMWJlZDUwNWUwY2Y2MzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2YIFbjc7_BFrYz_skVjFzF-IGoeQ7Ku4RmjwUtXsLdE'
-                    }
-                }
-                const fetchedData = fetch(url, options)
-                    .then(res => res.json())
-                    .then(json => console.log(json))
-                    .catch(err => console.error(err))
-                setData(fetchedData)
-                console.log('Fetched movie data from API:', fetchedData)
-                localStorage.setItem('movie-database', JSON.stringify())
-            } catch {
-                console.error(err)
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        fetchMovieData(searchedMovie)
-    }, [searchedMovie])
-
-    if (loading || !data) {
-        return (
-            <div>
-                <h4>Loading...</h4>
-            </div>
-        )
+      //     fetch(url, options)
+      //       .then((res) => res.json())
+      //       .then((json) => console.log(json))
+      //       .catch((err) => console.error(err));
+      //   }
     }
 
+    fetchMovieData(searchedMovie);
+  }, [searchedMovie]);
+
+  if (loading || !data) {
     return (
-        <main className="flex flex-wrap justify-center max-w-[1368px] gap-10">
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-        </main>
-    )
+      <div>
+        <h4>Loading...</h4>
+      </div>
+    );
+  }
+
+  return (
+    <main className="flex flex-wrap justify-center max-w-[1368px] gap-10">
+      {detail && (
+        <Modal
+          handleCloseModal={() => {
+            setDetail(null);
+          }}
+        >
+          {" "}
+          {/* Do Modal only if detail is true */}
+          <div>
+            <h6>Name</h6>
+            <h2>{detail.original_title}</h2>
+          </div>
+          <div>
+            <h6>Description</h6>
+            <p>{detail.overview}</p>
+          </div>
+        </Modal>
+      )}
+      {data.results.map((movieData, movieDataIndex) => {
+        return (
+          <MovieCard
+            key={movieDataIndex}
+            movieData={movieData}
+            setDetail={setDetail}
+            detail={detail}
+          />
+        );
+      })}
+    </main>
+  );
 }
