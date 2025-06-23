7da81dc3c86593929deca2017136c4b6008d537d
Peter Burza <peterburza48@gmail.com>
2025-06-23 06:21:31 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import SearchBar from "./components/SearchBar"
 import Movies from "./components/Movies"
 import Footer from "./components/Footer"
+import { useState } from "react"
 
 function App() {
+  const [searchedMovie, setSearchedMovie] = useState('Spider-man')
 
   return (
     <>
-      <SearchBar />
-      <Movies />
+      <SearchBar searchedMovie={searchedMovie} setSearchedMovie={setSearchedMovie} />
+      <Movies searchedMovie={searchedMovie} />
       <Footer />
     </>
   )
